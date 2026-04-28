dce993ddb007c3a545a65092751491c2d5e6d6b1
Elin Ronda <elro2506@student.miun.se>
2026-04-28 12:15:31 +0200

    Fixat konverterarna

--- src/app/pages/converter/converter.ts
+++ src/app/pages/converter/converter.ts
@@ -10,8 +10,31 @@ import { FormsModule } from '@angular/forms';
 export class Converter {
 
   //Properties
-  name : string= "Elin Ronda";
-  email : string= "elro2506@student.miun.se";
-  isStudent: boolean = true;
+  meter: number = 0;
+  feet: number = 0;
+  result1: number = 0;
+  result2: number = 0;
+  result3: number = 0;
+  result4: number = 0;
+  celsius: number = 0;
+  fahrenheit: number = 0;
+
+  //Metoder
+  toMeter() : void {
+this.result1 = this.feet / 3.28;
+  }
+
+  toFeet() : void {
+this.result2 = this.meter * 3.28;
+  }
+
+  toCelsius() : void {
+this.result4 = parseFloat(((this.fahrenheit - 32) / 1.8).toFixed(2));
+  }
+
+  toFahrenheit() : void {
+this.result3 = (this.celsius * 1.8) + 32;
+  }
 }
+
 
